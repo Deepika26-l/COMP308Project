d7b0ea1e683008298455f267436662708440e16c
Arthur Moraes <ivson.ma@gmail.com>
2021-04-13 19:10:07 -0400

    Added Signup Form and Menu

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,5 +1,8 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Link, Redirect } from "react-router-dom";
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
 import Home from "./components/Home";
 import Signup from "./components/Signup";
 import Login from "./components/Login";
@@ -8,20 +11,53 @@ import Alert from "./components/Alert";
 import ListVitalSigns from "./components/ListVitalSigns";
 import MotivationalTips from "./components/MotivationalTips";
 
-const App = () => (
-  <BrowserRouter>
-    <div className="container">
-      <Switch>
-        <Route component={Home} path="/" exact={true} />
-        <Route component={Signup} path="/signup" />
-        <Route component={Login} path="/login" />
-        <Route component={CreateVitalSigns} path="/" />
-        <Route component={ListVitalSigns} path="/" />
-        <Route component={Alert} path="/" />
-        <Route component={MotivationalTips} path="/" />
-      </Switch>
-    </div>
-  </BrowserRouter>
-);
+
+function App(){
+  return(
+    <Router>
+
+      <Navbar>
+        <Navbar.Collapse>
+          <Nav>
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/signup">Signup</Nav.Link>
+            <Nav.Link href="/login">Login</Nav.Link>
+            <Nav.Link href="/">CreateVitalSigns</Nav.Link>
+            <Nav.Link href="/">ListVitalSigns</Nav.Link>
+            <Nav.Link href="/">MotivationalTips</Nav.Link>
+            <Nav.Link href="/">Alert</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+
+      <div className="container">
+        <Route render = {() => <Home/>} path="/" />
+        <Route render = {() => <Signup/>} path="/" />
+        <Route render = {() => <Login/>} path="/" />
+        <Route render = {() => <CreateVitalSigns/>} path="/" />
+        <Route render = {() => <ListVitalSigns/>} path="/" />
+        <Route render = {() => <MotivationalTips/>} path="/" />
+        <Route render = {() => <Alert/>} path="/" />
+      </div>
+    </Router>
+  );
+}
+
+
+// const App = () => (
+//   <BrowserRouter>
+//     <div className="container">
+//       <Switch>
+//         <Route component={Home} path="/" exact={true} />
+//         <Route component={Signup} path="/signup" />
+//         <Route component={Login} path="/login" />
+//         <Route component={CreateVitalSigns} path="/" />
+//         <Route component={ListVitalSigns} path="/" />
+//         <Route component={Alert} path="/" />
+//         <Route component={MotivationalTips} path="/" />
+//       </Switch>
+//     </div>
+//   </BrowserRouter>
+// );
 
 export default App;
---
+++ react-ui/src/components/Signup.js
@@ -0,0 +1,201 @@
+import React, { useState } from "react";
+import { Form, Button, Container, Spinner } from "react-bootstrap";
+
+
+const Signup = props => {
+
+    const [state, setState] = useState({
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+        userType: ""
+    });
+
+    //
+    const [showLoading, setShowLoading] = useState(false);
+
+    const handleOnSubmit = event => {
+        setShowLoading(true);
+        event.preventDefault();
+        console.log(state);
+            props.history.push({
+            pathname: "/",          /*********TBD*********/
+            state
+            });
+    };
+
+  const onChange = e => {
+    e.persist();
+    setState({ ...state, [e.target.name]: e.target.value });
+  };
+  //
+  return (
+    <div>
+      <Container>
+        <div className="text-center">
+          <div
+            style={{
+              position: "absolute",
+              top: "3vw",
+              bottom: "0",
+              left: "0",
+              right: "0",
+              margin: "auto",
+              width: "30vw",
+              height: "auto",
+            }}
+          >
+            <h1 
+                style={{
+                    textAlign: "center", 
+                    color: "blueviolet"
+                }}>
+                Prediction Data
+            </h1>
+            {showLoading && (
+              <Spinner animation="border" role="status"></Spinner>
+            )}
+            <Form onSubmit={handleOnSubmit}>
+                <Form.Group>
+                    <Form.Label>First Name: </Form.Label>
+                    <Form.Control
+                    style={{
+                        width: "100%",
+                        padding: "12px 20px",
+                        margin: "8px 0",
+                        display: "inline-block",
+                        border: "1px solid #ccc",
+                        borderRadius: "4px",
+                        boxSizing: "border-box",
+                    }}
+                    type="text"
+                    step=".1"
+                    name="firstName"
+                    id="firstName"
+                    placeholder="John"
+                    value={state.firstName}
+                    onChange={onChange}
+                    required
+                    />
+                </Form.Group>
+                {/* */}
+                <Form.Group>
+                    <Form.Label>Last Name: </Form.Label>
+                    <Form.Control
+                    style={{
+                        width: "100%",
+                        padding: "12px 20px",
+                        margin: "8px 0",
+                        display: "inline-block",
+                        border: "1px solid #ccc",
+                        borderRadius: "4px",
+                        boxSizing: "border-box",
+                    }}
+                    type="text"
+                    step=".1"
+                    name="lastName"
+                    id="lastName"
+                    placeholder="Doe"
+                    value={state.lastName}
+                    onChange={onChange}
+                    required
+                    />
+                </Form.Group>
+                {/* */}
+                <Form.Group>
+                    <Form.Label>Email: </Form.Label>
+                    <Form.Control
+                    style={{
+                        width: "100%",
+                        padding: "12px 20px",
+                        margin: "8px 0",
+                        display: "inline-block",
+                        border: "1px solid #ccc",
+                        borderRadius: "4px",
+                        boxSizing: "border-box",
+                    }}
+                    type="email"
+                    step=".1"
+                    name="email"
+                    id="email"
+                    placeholder="my@mail.com"
+                    value={state.email}
+                    onChange={onChange}
+                    required
+                    />
+                </Form.Group>
+                {/* */}
+                <Form.Group>
+                    <Form.Label>Password: </Form.Label>
+                    <Form.Control
+                    style={{
+                        width: "100%",
+                        padding: "12px 20px",
+                        margin: "8px 0",
+                        display: "inline-block",
+                        border: "1px solid #ccc",
+                        borderRadius: "4px",
+                        boxSizing: "border-box",
+                    }}
+                    type="password"
+                    step=".1"
+                    name="password"
+                    id="password"
+                    placeholder="******"
+                    value={state.password}
+                    onChange={onChange}
+                    required
+                    />
+                </Form.Group>
+                {/* */}
+                <Form.Group>
+                    <Form.Label>User Type: </Form.Label>
+                    <Form.Control
+                        style={{
+                            width: "100%",
+                            padding: "12px 20px",
+                            margin: "8px 0",
+                            display: "inline-block",
+                            border: "1px solid #ccc",
+                            borderRadius: "4px",
+                            boxSizing: "border-box",
+                        }}
+                        as="select"
+                        step="any"
+                        name="userType"
+                        id="UserType"
+                        value={state.userType}
+                        onChange={onChange}
+                        required
+                    >
+                        <option value="Patient">Patient</option>
+                        <option value="Nurse">Nurse</option>
+                    </Form.Control>
+                </Form.Group>                
+                <Button
+                    style={{
+                    border: "none",
+                    color: "white",
+                    padding: "15px 32px",
+                    textAlign: "center",
+                    textDecoration: "none",
+                    display: "inline-block",
+                    fontSize: "15px",
+                    fontWeight: "bold",
+                    margin: "10px",
+                    backgroundColor: "blueviolet",
+                    }}
+                    variant="primary"
+                    type="submit"
+                >
+                    Submit
+                </Button>
+            </Form>
+          </div>
+        </div>
+      </Container>
+      </div>
+  );
+};
+export default Signup;
