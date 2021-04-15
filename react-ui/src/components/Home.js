0c23c5e71e201de932ea98f28a896fdb4178214d
Deepika <deepikakalra61@gmail.com>
2021-04-15 02:47:52 -0400

    changes in Home page

--- react-ui/src/components/Home.js
+++ react-ui/src/components/Home.js
@@ -1,22 +1,32 @@
 import { withRouter } from 'react-router-dom';
+import React, { useState } from "react";
+import { Form, Button, Container, Spinner } from "react-bootstrap";
+//import Jumbotron from 'react-bootstrap/Jumbotron';
 
-import React, { Component }  from 'react';
-import Jumbotron from 'react-bootstrap/Jumbotron';
-
-function Home(props)
+function Home()
 {
 
     return (
         <div>
-            <h2>Patient-Nurse Application</h2>
-            <Jumbotron>
-            
-            <h4>
-                <a href="/login">Login</a> or <a href="/signup">Sign Up</a>
-            </h4>
-            <br/>
-            <img src="medical.png.jpg" width="915"></img>
-            </Jumbotron>
+            <div>
+             <img align="center" src="medical.png" width="100"></img>
+            </div>
+            <Container>
+              <h2 style={{
+                    textAlign: "center", 
+                    color: "blueviolet"
+                }}>Patient-Nurse Application</h2>
+                
+        
+               <h4 style={{
+                   textAlign:"center",
+                   color:"red"
+               }}>
+                   <a href="/login">Login</a> or <a href="/signup">Sign Up</a>
+               </h4>
+               <br/>
+               
+            </Container>
         </div>
     );
 
