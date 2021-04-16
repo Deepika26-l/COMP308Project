c70c7633fbbe3187f0bd83fc96cc59ceb0d5fd16
Deepika <deepikakalra61@gmail.com>
2021-04-16 01:20:48 -0400

    proxy error resolved

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -6,51 +6,54 @@ import {
   Link,
   Redirect
 } from "react-router-dom";
-
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import './App.css';
-
-import Home from './components/Home';
+import FirstPage from './components/CustomerFirstPage';
+import AdminPage from './components/adminFirstPage';
 import Login from './components/Login';
-import Signup from './components/Signup';
-// import Videos from './components/Videos';
-// import ListPatients from './components/ListPatients';
-// import DisplayPatient from './components/DisplayPatient';
-// import DisplayAlert from './components/DisplayAlert';
-// import CreateEmergencyAlert from './components/CreateEmergencyAlert';
-// import AlertHistory from './components/AlertHistory';
-
-
+import SignUp from './components/SignUp'
+import Home from './components/Home';
+import OtpPage from './components/OtpPage';
+import CreatePackage from './components/CreatePackage';
+import ViewPackage from './components/ViewPackage';
+import ViewCustomers from './components/ViewCustomers';
+import SendAttractivePackage from './components/SendAttractivePackage'
+import Contact from './components/Contact';
+import CustomerViewPackages from './components/CustomerViewPackages';
+import CreateEmergency from './components/CreateEmergency';
+import EnterParameters from './components/EnterParameters';
 function App() {
 
   return (
-    <Router>
-      {/* <Navbar bg="light" expand="lg">
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="/home">Home</Nav.Link>
-            <Nav.Link href="/login">Login</Nav.Link>
-            <Nav.Link href="/signup">Register</Nav.Link>   
-            {/* <Nav.Link href="/alertshistory">Alert History</Nav.Link>   
-            <Nav.Link href="/videos">Videos</Nav.Link>          */}
-        
+  <div>
+      <Router>
     
-      <div>          
-          <Route render ={()=> < Home />} path="/Home" />
-          <Route render ={()=> < Login />} path="/Login" />
-          <Route render ={()=> < Signup />} path="/Signup" /> 
-          {/* <Route render ={()=> < Videos />} path="/videos" /> 
-          <Route render ={()=> < ListPatients />} path="/patients" />
-          <Route render ={()=> < DisplayPatient />} path="/patient/:id" />
-          <Route render ={()=> < AlertHistory />} path="/alertshistory" />                  
-          <Route render ={()=> < DisplayAlert />} path="/alerthistory/:id" />
-          <Route render ={()=> < CreateEmergencyAlert />} path="/alerts" /> */}
-          
-      </div>
+       <div>
+       <Switch>
+       
+       <Route render ={()=> < CreateEmergency/>} path="/creteAnEmergencyAlert" />
+       
+       <Route render ={()=> < EnterParameters/>} path="/enterParameters" />
+       <Route render ={()=> < CustomerViewPackages/>} path="/customerViewPackages" />
+       <Route render ={()=> < SendAttractivePackage/>} path="/SendMotivationalTips" />
+       <Route render ={()=> < ViewCustomers/>} path="/ViewCustomers" />
+       <Route render ={()=> < ViewPackage/>} path="/ViewPackage" />
+       <Route render ={()=> < CreatePackage/>} path="/CreatePackage" />
+       <Route render ={()=> < AdminPage/>} path="/AdminFirstPage" />
+       <Route render ={()=> < OtpPage/>} path="/AdminOtpPage" />
+       <Route render ={()=> < Contact/>} path="/Contact" />
+       <Route render ={()=> < FirstPage />} path="/FirstPage" />
+       <Route render ={()=> < Login />} path="/Login" />
+       <Route render ={()=> < SignUp />} path="/register" />
+       <Route render ={()=> < Home/>} path="/" />
+       
 
-    </Router>
+       </Switch>
+      
+       </div>
+       </Router>
+ </div>
 
 
   );
